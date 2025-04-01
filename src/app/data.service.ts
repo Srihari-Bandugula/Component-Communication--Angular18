12d9fa7d678c776ee7f9fce9917984b676fcbae1
Srihari-Bandugula <165921820+Srihari-Bandugula@users.noreply.github.com>
2025-04-01 11:08:13 +0530

    Initial commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+  person={
+    'name':'John',
+    'gender':'Male',
+    'Loc':'Hyderabad',
+    'purpose':'By using Service'
+  }
+
+}
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,8 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChildComponent } from "../child/child.component";
-import { log } from 'node:util';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-parent',
   imports: [FormsModule, ChildComponent],
@@ -13,7 +12,12 @@ export class ParentComponent {
   name:string='';
   gMail:any='';
   mOrF:string='';
+  personData: any;
   @ViewChild(ChildComponent) G:any;
+
+  constructor(private dataService:DataService){
+    this.personData=this.dataService.person;
+  }
   ngAfterViewChecked() {
     this.mOrF = this.G.gender;
   }
